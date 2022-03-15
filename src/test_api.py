957180b350d49b716a8da6ea6758e701e0379aee
SaraPetre <sara.petre@chasacademy.se>
2022-03-15 13:18:00 +0000

    Deleted one test in test_api.py

--- src/test_api.py
+++ src/test_api.py
@@ -15,19 +15,3 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello, World!"}
-
-
-#def test_stores_name():
- #   """D
-
-<<<<<<< HEAD
-  #  """
-   # response = client.get("/stores/{specifik}")
-    #assert response.status_code == 404
-    #assert response.json() == {"detail": "Not Found!"}
-=======
-    """
-    response = client.get("/stores")
-    assert response.status_code == 404
-    assert response.json() == {"detail": "Not Found!"}
->>>>>>> 1777de5fbd4f549a5893e625d7add9ef650a80ff
