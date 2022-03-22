16257de772d0428d3c33c3e68c4c592fb2fa0c98
Malik Omar <abdimalik08@icloud.com>
2022-03-22 20:46:10 +0000

    Update src/api.py

--- src/api.py
+++ src/api.py
@@ -1,37 +1,133 @@
-"""api-app
-"""
+"Time to add info here Sara"
 
-
-from fastapi import FastAPI
+# from unittest.mock import MagicMock
+from fastapi import FastAPI, HTTPException
 
 import psycopg
 
 app = FastAPI()
 
 
+@app.get("/")
+def read_main():
+    """The root endpoint, simply says hi to the user :)
+
+
+    """
+    return {"msg": "Hello, World!"}
+
+
+# class CursorMock(MagicMock):
+    # """
+    # D
+    # """
+    # fetchall = MagicMock(return_value=[[1, "a", "b"]])
+
+
+# class DBMock:
+    # """
+    # D
+    # """
+    # cursor = CursorMock
+
+
 @app.on_event("startup")
 def startup():
-    """
-    a function that should be run before the application starts.
-    """
-    app.db = psycopg.connect("postgresql://postgres:testpass@localhost/postgres")
+    "D"
+
+    app.db = psycopg.connect(
+        """dbname=postgres user=postgres host=localhost password=arastest port=5433""")
 
 
 @app.on_event("shutdown")
 def shutdown():
-    """
-    a function that should be run when the application is shutting down.
-    """
+    "D"
     app.db.close()
+
+
+@app.get("/stores/{specifik}")
+def specific_store(specifik):
+    '''
+    returnera data (namn och fullständig address) för en specifik
+    butik, vald via namn om ett namn som inte finns i DB anges,
+    returnera 404 Not Found
+    '''
+
+    with app.db.cursor() as cur:
+        cur.execute("""select stores.name, store_addresses.address, store_addresses.zip,
+                    store_addresses.city
+                    from stores
+                    join store_addresses
+                    on stores.id = store_addresses.store where name = %s;""", [specifik])
+        sname = cur.fetchall()
+        if sname:
+            sname = sname[0]
+            result = {"data": {"name": sname[0], "address": f"{sname[1]}, {sname[2]} {sname[3]}"}}
+            return result
+        else:
+            raise HTTPException(status_code=404, detail=f"Store {specifik} not found!")
 
 
 @app.get("/stores")
 def stores():
-    """
-    Retrieves a resource from the database.
-    """
+    '''
+    This endpoint returns data on stores (name and complete adress)
+    '''
+
     with app.db.cursor() as cur:
-        cur.execute("SELECT name FROM stores")
+        cur.execute("""select stores.name, store_addresses.address, store_addresses.zip,
+                    store_addresses.city
+                    from stores
+                    join store_addresses on stores.id = store_addresses.store""")
+        for record in cur:
+            print(record)
+    with app.db.cursor() as cur:
+        cur.execute("""select stores.name, store_addresses.address, store_addresses.zip,
+                    store_addresses.city
+                    from stores
+                    join store_addresses on stores.id = store_addresses.store""")
+        data = cur.fetchall()
+        data = [{"name": d[0], "address": f"{d[1]}, {d[2]} {d[3]}"} for d in data]
+        result = {"data": data}
+        return result
+
+
+@app.get("/cities")
+def city(zip=None):
+    '''
+    Denna endpoint returnerar alla unika städer där en butik finns.
+
+    Om query-parameter zip är given, den filtrerar med den och visar
+    bara städer med den specifika postkod.
+    '''
+    with app.db.cursor() as cur:
+        if not zip:
+            cur.execute("SELECT DISTINCT city FROM store_addresses;")
+        else:
+            cur.execute("SELECT city FROM store_addresses WHERE zip = %s;", [zip])
         names = cur.fetchall()
-        names = [name[0] for name in names]
-        return names
+        result = {"data": [name[0] for name in names]}
+        return result
+
+
+@app.get("/sales")
+def sales():
+    '''
+    GET /sales : Denna endpoint ska returnera en lista över alla transak-
+    tioner, i denna format:
+    {”data” : [
+    {”store” : ”Store name” ,
+    ”timestamp” : ”yyyymmdd hh:mm:ss” ,
+    ”saleid” : ”uuid for the transaction here”},
+    ...
+    ]
+}
+    '''
+    with app.db.cursor() as cur:
+        cur.execute("""SELECT stores.name, sales.time, sales.store
+                    FROM stores
+                    INNER JOIN sales
+                    ON stores.id = sales.store;""")
+        data = cur.fetchall()
+        data = {"data": [{"store": d[0], "timestamp": d[1], "saleid": d[2]} for d in data]}
+        return data
