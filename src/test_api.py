960b3ccadaa7cc6e9395277bb1b25d0e30f33830
Malik Omar <abdimalik08@icloud.com>
2022-03-22 20:56:48 +0000

    Update src/test_api.py

--- src/test_api.py
+++ src/test_api.py
@@ -46,8 +46,38 @@ def test_specific_store_not_in_list():
     startup()
     # app.db = DBMock()
     response = client.get("/stores/ArasDjuraffär")
-    # assert response.status_code == 200
-    # assert response.json() == [[1, "a", "b"]]
     assert response.status_code == 404
-    assert response.json() == {'detail': 'Store ArasDjuraffär not found!'}
+    assert response.json() == {"detail": 'Store ArasDjuraffär not found!'}
+    shutdown()
+def test_stores_in_list():
+    '''
+    Testen returnera data från listan för att se till att vi får vad vi förväntar oss
+    '''
+    startup()
+    response = client.get("/stores/Djurjouren")
+    assert response.status_code == 200
+    assert response.json() == {"detail": {"name": "Djuristen", "address": "Upplandsgatan 99, 12345 Stockholm"}}
+    shutdown()
+
+
+def test_sales_exist():
+    ''' 
+    testen returnera sales ID för att säkerställa att vi får vad vi förväntar oss
+    '''
+    startup()
+    response = client.get("/sales/726ac398-209d-49df-ab6a-682b7af8abfb")
+    assert response.status_code == 200
+    assert response.json() == {
+    "detail": {"store": "Djuristen","timestamp": "2022-01-26T15:24:45","sale_id": "0188146f-5360-408b-a7c5-3414077ceb59",}}
+    shutdown()
+
+
+def test_sales_not_exist():
+    ''' 
+     Testen returnera felmeddelande om sale id inte existerar
+    '''
+    startup()
+    response = client.get("/sales/0878158f-5360-408b-a7c5-3414077ceb90")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "this ID does not exist in the list"}
     shutdown()
