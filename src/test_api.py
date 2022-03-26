ba675461e8d582c7c9a2603967743e763e9e0541
“jani.karhula” <jani.karhula@chasacademy.se>
2022-03-26 17:37:49 +0100

    edit test_api

--- src/test_api.py
+++ src/test_api.py
@@ -140,28 +140,28 @@ def test_sales():
     response = client.get("/sales")
     assert response.status_code == 200
     assert response.json() == {
-      "data": [
-        {
-          "store": "Den Stora Djurbutiken",
-          "timestamp": "20220125T13:52:34",
-          "sale_id": "0188146f-5360-408b-a7c5-3414077ceb59"
-        },
-        {
-          "store": "Djuristen",
-          "timestamp": "20220126T15:24:45",
-          "sale_id": "726ac398-209d-49df-ab6a-682b7af8abfb"
-        },
-        {
-          "store": "Den Lilla Djurbutiken",
-          "timestamp": "20220207T09:00:56",
-          "sale_id": "602fbf9d-2b4a-4de2-b108-3be3afa372ae"
-        },
-        {
-          "store": "Den Stora Djurbutiken",
-          "timestamp": "20220227T12:32:46",
-          "sale_id": "51071ca1-0179-4e67-8258-89e34b205a1e"
-        }
-      ]
+        "data": [
+            {
+              "store": "Den Stora Djurbutiken",
+              "timestamp": "20220125T13:52:34",
+              "sale_id": "0188146f-5360-408b-a7c5-3414077ceb59"
+            },
+            {
+              "store": "Djuristen",
+              "timestamp": "20220126T15:24:45",
+              "sale_id": "726ac398-209d-49df-ab6a-682b7af8abfb"
+            },
+            {
+              "store": "Den Lilla Djurbutiken",
+              "timestamp": "20220207T09:00:56",
+              "sale_id": "602fbf9d-2b4a-4de2-b108-3be3afa372ae"
+            },
+            {
+              "store": "Den Stora Djurbutiken",
+              "timestamp": "20220227T12:32:46",
+              "sale_id": "51071ca1-0179-4e67-8258-89e34b205a1e"
+            }
+        ]
     }
     shutdown()
 
