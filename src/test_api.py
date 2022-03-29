1753a7a3d272628321aa065ae4c01c684b8a10f1
SaraPetre <sara.petre@chasacademy.se>
2022-03-29 20:13:41 +0000

    Sales format fixed #2

--- src/test_api.py
+++ src/test_api.py
@@ -175,17 +175,15 @@ def test_sales_id_valid():
     assert response.status_code == 200
     assert response.json() == {
         "data": {
-            {
-                "store": "Djuristen",
-                "timestamp": "2022-01-26T15:24:45",
-                "saleid": "726ac398-209d-49df-ab6a-682b7af8abfb",
-                "products": [
-                    {
-                        "name": "Elefantkoppel",
-                        "qty": 1
-                    }
-                ]
-            }
+            "store": "Djuristen",
+            "timestamp": "2022-01-26T15:24:45",
+            "saleid": "726ac398-209d-49df-ab6a-682b7af8abfb",
+            "products": [
+                {
+                    "name": "Elefantkoppel",
+                    "qty": 1
+                }
+            ]
         }
     }
 
